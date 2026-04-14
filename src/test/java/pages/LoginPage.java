e3c1e14817ea27c324c786c518a440844b6e58e5
Eloy Jiménez Medina <ejm0005@alu.medac.es>
2026-04-14 17:25:47 +0200

    Creación de estructura de LoginPage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,5 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
+    private WebDriver driver;
 
+    private By userField = By.id("user-name");
+    private By passField = By.id("password");
+    private By loginBtn = By.id("login-button");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void ingresarUsuario(String user) {
+        // escribe el usuario en el campo correspondiente
+    }
+
+    public void ingresarPassword(String pass) {
+        // escribe la contraseña en el campo correspondiente
+    }
+
+    public void clickLogin() {
+        // pulsa el botón de login
+    }
 }
